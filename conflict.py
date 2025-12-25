a8595517312c96f407badcac25bcc85d85c8328a
Nat Arrazola <NatyArrazola13@gmail.com>
2025-12-25 16:42:18 -0600

    nuevos cambios desde dev

--- conflict.py
+++ conflict.py
@@ -1,4 +1,7 @@
 # Este archivo es para practica y saber solucionar
 # conflictos en git, asi que aqui veremos esos temas
 
-print
+print("Hola, este es un archivo para practicar conflictos en git.")
+
+#Agregar nueva linea para generar conflicto
+print("Modificacion desde la rama dev.")
